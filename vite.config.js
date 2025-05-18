a51e79b44d91dc67e4423983e397de2dbba3be31
Trung Nghia <nh0cbum1234@gmail.com>
2025-05-18 11:27:20 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { ViteEjsPlugin } from "vite-plugin-ejs";
 import path from "path";
 
 export default defineConfig ({
+    base: "/f8-zoom-module-1",
     root: "src",
     publicDir: "../public",
     build: {
@@ -11,6 +12,11 @@ export default defineConfig ({
         rollupOption: {
             input:{
                 index: path.resolve(__dirname, "src/index.html"),
+                signup: path.resolve(__dirname, "src/sign-up.html"),
+                signin: path.resolve(__dirname, "src/sign-in.html"),
+                collection: path.resolve(__dirname, "src/collection.html"),
+                payment: path.resolve(__dirname, "src/payment.html"),
+                account: path.resolve(__dirname, "src/account.html"),
             },
         },
     },
